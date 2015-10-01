c1bd5c97e940d69642da424c3de506b9dcabbb64
Bob_Kupfner <azuesflip@gmail.com>
2015-10-01 01:16:02 -0400

    Initial commit. hello.c done and starting on mario.c

---
+++ mario.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    printf("We need to build a staircase for mario to climb. All that we need to decide is how high the staircase will rise. It just can't be larger than 23 nor smaller than 1 step!\n");git remote add origin https://github.com/Azuesflip/pset1.git
+}
