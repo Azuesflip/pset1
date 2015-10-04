91bcff4e49a615781dc128df700bc4ab6faef7ed
Bob_Kupfner <azuesflip@gmail.com>
2015-10-04 00:32:54 -0400

    mario.c is completed just need to document the file and make greedy.c

--- mario.c
+++ mario.c
@@ -3,5 +3,27 @@
 
 int main(void)
 {
-    printf("We need to build a staircase for mario to climb. All that we need to decide is how high the staircase will rise. It just can't be larger than 23 nor smaller than 1 step!\n");git remote add origin https://github.com/Azuesflip/pset1.git
+    int height;
+    
+    printf("We need to build a staircase for mario to climb. All that we need to decide is how high the staircase will rise. It just can't be larger than 23 nor smaller than 1 step!\n");
+    
+    do
+    {
+        printf("Please give a hieght for the staircase: \n");
+        height = GetInt();
+    }
+    while(height > 23 || height < 0)
+    {
+        for(i = 0; i < height; i++)
+        {
+            for(j = 1; j < (height - i); j++)
+            {
+                printf(" ");
+            }
+            for(k = 0; k < (i + 2); k++)
+            {
+                printf(
+            }
+        }
+    }
 }
